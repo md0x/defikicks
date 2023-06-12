9771fef33fddf6a2e3dcba1760c37a072a7b7856
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2023-06-12 07:19:13 +0100

    fix: ci bis
    Signed-off-by: Pablo Maldonado <pablomaldonadoturci@gmail.com>

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -7,22 +7,28 @@ import { resolve } from "path"
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env"
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) })
 
+const walletPrivateKey: string | undefined = process.env.WALLET_PRIVATE_KEY
+
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {},
 
-        filecoinCalibrationNet: {
-            url: "https://api.calibration.node.glif.io/rpc/v0",
-            chainId: 314159,
-            accounts: [process.env.WALLET_PRIVATE_KEY || ""],
-        },
-        filecoinMainnet: {
-            url: "https://api.node.glif.io", //'https://rpc.ankr.com/filecoin_testnet', //https://filecoin-hyperspace.chainstacklabs.com/rpc/v1
-            chainId: 314,
-            accounts: [process.env.WALLET_PRIVATE_KEY || ""],
-        },
+        ...(walletPrivateKey
+            ? {
+                  filecoinCalibrationNet: {
+                      url: "https://api.calibration.node.glif.io/rpc/v0",
+                      chainId: 314159,
+                      accounts: [walletPrivateKey],
+                  },
+                  filecoinMainnet: {
+                      url: "https://api.node.glif.io", //'https://rpc.ankr.com/filecoin_testnet', //https://filecoin-hyperspace.chainstacklabs.com/rpc/v1
+                      chainId: 314,
+                      accounts: [walletPrivateKey],
+                  },
+              }
+            : {}),
     },
 }
 
